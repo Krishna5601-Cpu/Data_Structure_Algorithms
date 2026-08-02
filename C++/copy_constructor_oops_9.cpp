fbe402642352651ebdc4890ed7cb5c2e7a690580
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-08-02 16:35:55 +0530

    Deep Copy

--- C++/copy_constructor_oops_9.cpp
+++ C++/copy_constructor_oops_9.cpp
@@ -1,33 +1,121 @@
+// #include <iostream>
+// using namespace std;
+
+// // class Student
+// // {
+// // public:
+// //   int *marks;
+
+// //   Student()
+// //   {
+// //     marks = new int(95);
+// //   }
+
+// //   ~Student()
+// //   {
+// //     delete marks;
+// //   }
+// // };
+
+// class Student
+// {
+// public:
+//   int *marks;
+
+//   Student(int m)
+//   {
+//     marks = new int(m);
+//   };
+
+//    Student(const Student &other)
+//   {
+//     *marks = &marks;
+//   }
+
+// };
+
+// // class Student
+// // {
+// // public:
+// //   string name;
+// //   int age;
+
+// //   Student(string n, int a)
+// //   {
+// //     name = n;
+// //     age = a;
+// //   }
+
+// //   void display()
+// //   {
+// //     cout << name << " " << age << endl;
+// //   }
+// // };
+
+// int main()
+// {
+//   // Student s1("Krishna", 20);
+
+//   // Student s2 = s1;
+
+//   // s2.name = "Rahul";
+
+//   // s1.display();
+
+//   // s2.display();
+
+//   //   Student s1;
+
+//   // Student s2 = s1;
+
+//   Student s1(95);
+
+//   Student s2 = s1;
+
+//   *s2.marks = 100;
+
+// }
+
 #include <iostream>
 using namespace std;
 
 class Student
 {
 public:
-  string name;
-  int age;
+  int *marks;
 
-  Student(string n, int a)
+  Student(int m)
   {
-    name = n;
-    age = a;
+    marks = new int(m);
+  };
+
+  Student(const Student &other)
+  {
+    marks = new int(*other.marks);
+  }
+
+  ~Student()
+  {
+    delete marks;
   }
 
   void display()
   {
-    cout << name << " " << age << endl;
+    cout << *marks << endl;
   }
 };
 
 int main()
 {
-  Student s1("Krishna", 20);
+  Student s1(95);
 
   Student s2 = s1;
 
-  s2.name = "Rahul";
+  *s2.marks = 100;
 
+  cout << "s1 = ";
   s1.display();
 
+  cout << "s2 = ";
   s2.display();
 }
