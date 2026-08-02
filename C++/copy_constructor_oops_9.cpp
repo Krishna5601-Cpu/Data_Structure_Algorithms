ed788c71de825220bc93ee1a5a44881e4d4be13c
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-08-02 16:45:28 +0530

    shallow & deep copy

--- C++/copy_constructor_oops_9.cpp
+++ C++/copy_constructor_oops_9.cpp
@@ -1,21 +1,83 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
+
+// // // class Student
+// // // {
+// // // public:
+// // //   int *marks;
+
+// // //   Student()
+// // //   {
+// // //     marks = new int(95);
+// // //   }
+
+// // //   ~Student()
+// // //   {
+// // //     delete marks;
+// // //   }
+// // // };
 
 // // class Student
 // // {
 // // public:
 // //   int *marks;
 
-// //   Student()
+// //   Student(int m)
 // //   {
-// //     marks = new int(95);
+// //     marks = new int(m);
+// //   };
+
+// //    Student(const Student &other)
+// //   {
+// //     *marks = &marks;
 // //   }
 
-// //   ~Student()
-// //   {
-// //     delete marks;
-// //   }
 // // };
+
+// // // class Student
+// // // {
+// // // public:
+// // //   string name;
+// // //   int age;
+
+// // //   Student(string n, int a)
+// // //   {
+// // //     name = n;
+// // //     age = a;
+// // //   }
+
+// // //   void display()
+// // //   {
+// // //     cout << name << " " << age << endl;
+// // //   }
+// // // };
+
+// // int main()
+// // {
+// //   // Student s1("Krishna", 20);
+
+// //   // Student s2 = s1;
+
+// //   // s2.name = "Rahul";
+
+// //   // s1.display();
+
+// //   // s2.display();
+
+// //   //   Student s1;
+
+// //   // Student s2 = s1;
+
+// //   Student s1(95);
+
+// //   Student s2 = s1;
+
+// //   *s2.marks = 100;
+
+// // }
+
+// #include <iostream>
+// using namespace std;
 
 // class Student
 // {
@@ -27,57 +89,36 @@
 //     marks = new int(m);
 //   };
 
-//    Student(const Student &other)
+//   Student(const Student &other)
 //   {
-//     *marks = &marks;
+//     marks = new int(*other.marks);
 //   }
 
+//   ~Student()
+//   {
+//     delete marks;
+//   }
+
+//   void display()
+//   {
+//     cout << *marks << endl;
+//   }
 // };
-
-// // class Student
-// // {
-// // public:
-// //   string name;
-// //   int age;
-
-// //   Student(string n, int a)
-// //   {
-// //     name = n;
-// //     age = a;
-// //   }
-
-// //   void display()
-// //   {
-// //     cout << name << " " << age << endl;
-// //   }
-// // };
 
 // int main()
 // {
-//   // Student s1("Krishna", 20);
-
-//   // Student s2 = s1;
-
-//   // s2.name = "Rahul";
-
-//   // s1.display();
-
-//   // s2.display();
-
-//   //   Student s1;
-
-//   // Student s2 = s1;
-
 //   Student s1(95);
 
 //   Student s2 = s1;
 
 //   *s2.marks = 100;
 
-// }
+//   cout << "s1 = ";
+//   s1.display();
 
-#include <iostream>
-using namespace std;
+//   cout << "s2 = ";
+//   s2.display();
+// }
 
 class Student
 {
@@ -87,21 +128,19 @@ public:
   Student(int m)
   {
     marks = new int(m);
-  };
+    cout << "Constructor\n";
+  }
 
   Student(const Student &other)
   {
     marks = new int(*other.marks);
+    cout << "Copy Constructor\n";
   }
 
   ~Student()
   {
     delete marks;
-  }
-
-  void display()
-  {
-    cout << *marks << endl;
+    cout << "Destructor\n";
   }
 };
 
@@ -111,11 +150,11 @@ int main()
 
   Student s2 = s1;
 
+  Student s3 = s2;
+
   *s2.marks = 100;
 
-  cout << "s1 = ";
-  s1.display();
-
-  cout << "s2 = ";
-  s2.display();
+  cout << *s1.marks << endl;
+  cout << *s2.marks << endl;
+  cout << *s3.marks << endl;
 }
