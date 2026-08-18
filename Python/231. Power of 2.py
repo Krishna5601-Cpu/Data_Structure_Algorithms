2e6fcf284a3d622c2c646dd6ea1b268971f17e87
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-08-19 01:02:29 +0530

    231. Power of 2 in python second method

--- Python/231. Power of 2.py
+++ Python/231. Power of 2.py
@@ -9,3 +9,5 @@ class Solution(object):
         while n % 2 == 0:
             n //= 2
         return n == 1
+    # Method 2: Bit manipulation (most efficient method)
+    # return n > 0 and (n & (n - 1)) == 0
