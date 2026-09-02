31bdad9ada4392bbf8328a0fb48815db8a86d12c
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-03 00:56:02 +0530

    OOPS_13: order of constructors and desconstructors in inheritance in cpp

---
+++ C++/oops_12.cpp
@@ -0,0 +1,43 @@
+// // Constructor Order in Inheritance
+
+// class Person
+// {
+// public:
+//   Person()
+//   {
+//     cout << "Person Constructor\n";
+//   }
+// };
+
+// class Student : public Person
+// {
+// public:
+//   Student()
+//   {
+//     cout << "Student Constructor\n";
+//   }
+// };
+
+// int main()
+// {
+//   Student s;
+// }
+
+// class Person
+// {
+// public:
+//   Person() { cout << "Person Constructor\n"; }
+//   ~Person() { cout << "Person Destructor\n"; }
+// };
+
+// class Student : public Person
+// {
+// public:
+//   Student() { cout << "Student Constructor\n"; }
+//   ~Student() { cout << "Student Destructor\n"; }
+// };
+
+// int main()
+// {
+//   Student s;
+// }
