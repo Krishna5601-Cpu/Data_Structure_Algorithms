3e454560855cdb291760ed67fdefe86f3268ad6c
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-19 00:28:41 +0530

    basic exception handling - 3 in cpp

--- C++/exception_handling.cpp
+++ C++/exception_handling.cpp
@@ -1,43 +1,86 @@
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main()
+// {
+//   int a, b;
+//   cin >> a >> b;
+
+//   try
+//   {
+//     if (b == 0)
+//       throw "Division by zero!";
+
+//     cout << a / b;
+//   }
+//   catch (const char *msg)
+//   {
+//     cout << msg;
+//   }
+
+//   try
+//   {
+//     throw 404;
+//   }
+//   catch (int x)
+//   {
+//     cout << "Integer Error: " << x;
+//   }
+//   catch (...)
+//   {
+//     cout << "Unknown Error";
+//   };
+
+//   ifstream file("students.txt");
+
+//   if (!file)
+//   {
+//     cout << "File not found!";
+//     return 0;
+//   }
+
+//   return 0;
+// }
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <stdexcept>
 using namespace std;
 
 int main()
 {
-  int a, b;
-  cin >> a >> b;
+  string name;
+  double marks;
+
+  // Ask user for name
+  cout << "Enter student name: ";
+  getline(cin, name);
+
+  // Ask user for marks
+  cout << "Enter student marks: ";
+  cin >> marks;
 
   try
   {
-    if (b == 0)
-      throw "Division by zero!";
+    // Check if marks are out of range
+    if (marks < 0 || marks > 100)
+    {
+      throw runtime_error("Invalid Marks");
+    }
 
-    cout << a / b;
-  }
-  catch (const char *msg)
-  {
-    cout << msg;
-  }
+    // Save student into students.txt
+    ofstream file("students.txt", ios::app);
+    file << name << "," << marks << endl;
+    file.close();
 
-  try
-  {
-    throw 404;
+    cout << "Saved Successfully" << endl;
   }
-  catch (int x)
+  catch (runtime_error &e)
   {
-    cout << "Integer Error: " << x;
-  }
-  catch (...)
-  {
-    cout << "Unknown Error";
-  };
-
-  ifstream file("students.txt");
-
-  if (!file)
-  {
-    cout << "File not found!";
-    return 0;
+    // Catch the exception and print the message
+    cout << e.what() << endl;
   }
 
   return 0;
