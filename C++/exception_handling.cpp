66d47ca600cb4f4b2b6216052820e1b900b947b9
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-19 00:23:05 +0530

    basic exception handling - 2 in cpp

--- C++/exception_handling.cpp
+++ C++/exception_handling.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -16,6 +17,27 @@ int main()
   catch (const char *msg)
   {
     cout << msg;
+  }
+
+  try
+  {
+    throw 404;
+  }
+  catch (int x)
+  {
+    cout << "Integer Error: " << x;
+  }
+  catch (...)
+  {
+    cout << "Unknown Error";
+  };
+
+  ifstream file("students.txt");
+
+  if (!file)
+  {
+    cout << "File not found!";
+    return 0;
   }
 
   return 0;
