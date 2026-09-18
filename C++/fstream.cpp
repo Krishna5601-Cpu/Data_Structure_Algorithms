368b1f37926557f6999f7680ebd66c34ad6787bf
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-18 18:59:41 +0530

    reading from a file in cpp

--- C++/fstream.cpp
+++ C++/fstream.cpp
@@ -2,14 +2,34 @@
 #include <fstream>
 using namespace std;
 
+// int main()
+// {
+//   ofstream file("students.txt");
+
+//   file << "Krishna 95\n";
+//   file << "Rahul 88\n";
+
+//   file.close();
+
+//   cout << "Data Saved!";
+
+// }
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 int main()
 {
-  ofstream file("students.txt");
+  ifstream file("students.txt");
 
-  file << "Krishna 95\n";
-  file << "Rahul 88\n";
+  string name;
+  int marks;
+
+  while (file >> name >> marks)
+  {
+    cout << name << " " << marks << endl;
+  }
 
   file.close();
-
-  cout << "Data Saved!";
-}
+};
