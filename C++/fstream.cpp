69b2e8c8fe4c4739e11e3b7e95ee690cb3bd107a
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-09-18 21:00:38 +0530

    ofstream, ifstream in cpp

--- C++/fstream.cpp
+++ C++/fstream.cpp
@@ -1,35 +1,67 @@
-#include <iostream>
-#include <fstream>
-using namespace std;
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// // int main()
+// // {
+// //   ofstream file("students.txt");
+
+// //   file << "Krishna 95\n";
+// //   file << "Rahul 88\n";
+
+// //   file.close();
+
+// //   cout << "Data Saved!";
+
+// // }
+
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
 
 // int main()
 // {
-//   ofstream file("students.txt");
+//   ifstream file("students.txt");
 
-//   file << "Krishna 95\n";
-//   file << "Rahul 88\n";
+//   string name;
+//   int marks;
+
+//   while (file >> name >> marks)
+//   {
+//     cout << name << " " << marks << endl;
+//   }
 
 //   file.close();
-
-//   cout << "Data Saved!";
-
-// }
+// };
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-  ifstream file("students.txt");
+  ofstream file("result.txt");
 
   string name;
   int marks;
 
-  while (file >> name >> marks)
+  cout << "Enter students data:\n";
+
+  for (int i = 1; i <= 3; i++)
   {
-    cout << name << " " << marks << endl;
+    cout << "\nStudent " << i << endl;
+
+    cout << "Name: ";
+    getline(cin >> ws, name);
+
+    cout << "Marks: ";
+    cin >> marks;
+
+    file << name << " " << marks << endl;
   }
 
   file.close();
-};
+
+  cout << "\nData saved successfully!";
+}
